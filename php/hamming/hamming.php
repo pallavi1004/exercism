a73c884f653320ff16ba6d193062910a8ac9064f
Tatsuya Kitabatake <e2tod2@gmail.com>
2018-03-03 13:35:41 +0900

    php hamming

---
+++ php/hamming/hamming.php
@@ -0,0 +1,21 @@
+<?php
+
+//
+// This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+function distance($a, $b)
+{
+    if (strlen($a) !== strlen($b)) {
+        throw new InvalidArgumentException("DNA strands must be of equal length.");
+    }
+
+    $count = 0;
+    for ($i = 0; $i < strlen($a); $i++) {
+        if ($a[$i] !== $b[$i]) {
+            $count++;
+        }
+    }
+    return $count;
+}
