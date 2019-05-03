8e60fea3b5d405500bb2b61f1360ed705bd502ff
Tatsuya Kitabatake <e2tod2@gmail.com>
2019-05-03 17:24:21 +0900

    refactor

--- php/markdown/markdown.php
+++ php/markdown/markdown.php
@@ -12,54 +12,47 @@ function parseMarkdown($markdown)
 
 function parseLine($line)
 {
-    list($match, $line) = parseHeader($line);
-    if (!$match) {
-        list($match, $line) = parseListItem($line);
-    }
-    if (!$match) {
+    if (!parseHeader($line) && !parseListItem($line)) {
         $line = "<p>{$line}</p>";
     }
-    return parsePartial($line);
-}
-
-function parsePartial($line)
-{
-    $line = preg_replace("/__(.+)__/", "<em>$1</em>", $line);
-    $line = preg_replace("/_(.+)_/", "<i>$1</i>", $line);
+    parsePartial($line);
     return $line;
 }
 
-function parseHeader($line)
+function parsePartial(&$line)
 {
-    if (!preg_match("/^#/", $line)) {
-     return [false, $line];
-    }
-
-    $count = 0;
-    foreach (str_split($line) as $char) {
-        if ($char === '#') {
-            $count++;
-        } else {
-            break;
-        }
-    }
-    return [true, sprintf("<h%d>%s</h%d>", $count, substr($line, $count + 1), $count)];
+    $line = preg_replace("/__(.+)__/", "<em>$1</em>", $line);
+    $line = preg_replace("/_(.+)_/", "<i>$1</i>", $line);
 }
 
-function parseListItem($line)
+function parseHeader(&$line)
+{
+    if (!preg_match("/^#/", $line)) {
+     return false;
+    }
+
+    $line = preg_replace_callback('/^(#+)\s+(.*)$/', function ($matches) {
+        $headLevel = strlen($matches[1]);
+        return "<h{$headLevel}>{$matches[2]}</h{$headLevel}>";
+    }, $line);
+
+    return true;
+}
+
+function parseListItem(&$line)
 {
     if (!preg_match("/^\*/", $line)) {
-        return [false, $line];
+        return false;
     }
 
-    $line = substr($line, 2);
+    $content = substr($line, 2);
     // adjust for strange test case
-    if (preg_match("/^_/", $line)) {
-        return [true,  "<li>" . $line . "</li>"];
+    if (preg_match("/^_/", $content)) {
+        $line =  "<li>" . $content . "</li>";
     } else {
-        return [true,  "<li><p>" . $line . "</p></li>"];
+        $line = "<li><p>" . $content . "</p></li>";
     }
-
+    return true;
 }
 
 function addUl($lines)
