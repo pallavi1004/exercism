7197f23a46245ba84c305fc04a33baf7b1afa854
Tatsuya Kitabatake <e2tod2@gmail.com>
2020-09-10 21:11:00 +0900

    refactor

--- kotlin/luhn/src/main/kotlin/Luhn.kt
+++ kotlin/luhn/src/main/kotlin/Luhn.kt
@@ -1,22 +1,23 @@
 object Luhn {
 
     fun isValid(candidate: String): Boolean {
-        val trimmed = candidate.trim().replace(" ", "")
-        if (trimmed.length <=1 || !trimmed.matches("\\d+".toRegex())) {
+        val arranged = candidate.trim().replace(" ", "")
+        if (arranged.length <= 1 || !arranged.matches("\\d+".toRegex())) {
             return false
         }
 
-        val ints = trimmed.toCharArray().map {Character.getNumericValue(it)}
-        println(ints.joinToString(" "))
-        val hoge = ints.reversed().mapIndexed { index, it ->
-            if (index % 2 == 1) {
-                val doubled = it * 2
-                if (doubled >= 10) doubled - 9 else doubled
-            } else {
-                it
-            }
-        }
+        val ints = arranged
+                .toCharArray()
+                .map { Character.getNumericValue(it) }
+                .reversed().mapIndexed { index, it ->
+                    if (index % 2 == 1) {
+                        val doubled = it * 2
+                        if (doubled >= 10) doubled - 9 else doubled
+                    } else {
+                        it
+                    }
+                }
 
-        return hoge.sum() % 10 == 0
+        return ints.sum() % 10 == 0
     }
 }
