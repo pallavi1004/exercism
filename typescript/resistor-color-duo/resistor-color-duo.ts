208d257ad7546ed41abb34110c2d95cfa7c5e9da
Tatsuya Kitabatake <e2tod2@gmail.com>
2021-03-02 09:05:07 +0900

    ts resistor color duo

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,27 @@
+const colorNumbers = new Map(
+    Object.entries({
+      black: 0,
+      brown: 1,
+      red: 2,
+      orange: 3,
+      yellow: 4,
+      green: 5,
+      blue: 6,
+      violet: 7,
+      grey: 8,
+      white: 9,
+    })
+)
+
 export class ResistorColor {
   private colors: string[]
 
   constructor(colors: string[]) {
+    if (colors.length <= 1) {
+      throw new Error('At least two colors need to be present')
+    }
     this.colors = colors
   }
-  value = (): number => 0
+
+  value = (): number => colorNumbers.get(this.colors[0])! * 10 + colorNumbers.get(this.colors[1])!
 }
