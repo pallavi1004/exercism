7f12b3949635936aa6d098df9b5825dc28bfc62d
Tatsuya Kitabatake <e2tod2@gmail.com>
2021-03-18 08:50:46 +0900

    ruby word count

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -4,13 +4,11 @@ class Phrase
   end
 
   def word_count
-    @phrase
-          .downcase
-          .scan(/[a-z0-9']+/)
-          .each_with_object({}) do |word, result|
-      word = word.sub(/^'([a-z0-9']+)'$/, "\\1")
-      result[word] = result.key?(word) ? result[word] + 1 : 1
-    end
+    @phrase.downcase
+           .scan(/[a-z0-9']+/)
+           .each_with_object({}) { |word, result|
+             word = word.sub(/^'([a-z0-9']+)'$/, "\\1")
+             result[word] = result.key?(word) ? result[word] + 1 : 1
+           }
   end
 end
-
